c87cc85578a20d1d26bdc41034ea17741921bd66
bimbelLB3R <bimbellb3r@gmail.com>
2025-09-14 10:33:32 +0800

    menambah metadata open graph

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,29 @@ const dancing = Dancing_Script({
 
 export const metadata = {
   title: "SATT",
-  description: "Sekolah Alam Tanjung Tabalong",
+  description: "Sekolah Alam Tanjung Tabalong (SATT) merupakan salah satu sekolah alam bagian dari JSAN (Jaringan Sekolah Alam Nasional). SATT berdiri sejak 2019 di kabupaten Tabalong. Sekolah ini menerapkan empat pilar utama, yaitu Pilar Akhlak, Pilar Logika, Pilar Kepemimpinan, dan Pilar Kewirausahaan.",
+  openGraph: {
+    title: "SATT - Belajar, Berpetualang, Bermakna",
+    description: "Merupakan sekolah alam yang menerapkan empat pilar utama, yaitu Pilar Akhlak, Pilar Logika, Pilar Kepemimpinan, dan Pilar Kewirausahaan.",
+    url: "https://sekolah-alam-tanjung-tabalong.vercel.app",
+    siteName: "SATT",
+    images: [
+      {
+        url: "https://sekolah-alam-tanjung-tabalong.vercel.app/og-image.png", // ganti dengan path gambarmu
+        width: 1200,
+        height: 630,
+        alt: "SATT - Belajar, Berpetualang, Bermakna",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SATT - Belajar, Berpetualang, Bermakna",
+    description: "Merupakan sekolah alam yang menerapkan empat pilar utama, yaitu Pilar Akhlak, Pilar Logika, Pilar Kepemimpinan, dan Pilar Kewirausahaan.",
+    images: ["https://sekolah-alam-tanjung-tabalong.vercel.app/og-image.png"], // gambar untuk Twitter
+  },
 };
 
 export default function RootLayout({ children }) {
