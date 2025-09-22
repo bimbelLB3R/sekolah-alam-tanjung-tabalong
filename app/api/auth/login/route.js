d4a9dc440c245306e2851661863ff672d55f14ac
bimbelLB3R <bimbellb3r@gmail.com>
2025-09-22 21:59:33 +0800

    import pool

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -2,13 +2,8 @@ import mysql from "mysql2/promise"
 import bcrypt from "bcryptjs"
 import { SignJWT } from "jose"
 import { cookies } from "next/headers"
+import pool from "@/lib/db"
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-})
 
 export async function POST(req) {
   try {
